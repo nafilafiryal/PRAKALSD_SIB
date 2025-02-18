fc8b76503ef39faf08a28c2d8c8a6cbf14a5ffb4
nafilafiryal <nafilahfiryal05@gmail.com>
2025-02-18 15:22:32 +0700

    Jobsheet II

---
+++ mahasiswa17.java
@@ -0,0 +1,43 @@
+public class mahasiswa17 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    public mahasiswa17() {
+
+    }
+    public mahasiswa17(String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+    void updateIpk(double ipkBaru) {
+        ipk = ipkBaru;
+    }
+    String nilaiKinerja() {
+        if (ipk < 0 && ipk > 4.0) {
+            return "IPK tidak valid. Harus antara 0.0 dan 4.0";
+        } else if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        }else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+}
