48517470b79cf9e815a653bdb7c9e560d330f35b
nafilafiryal <nafilahfiryal05@gmail.com>
2025-05-13 13:09:56 +0700

    Modifikasi Pertanyaan 3 Percobaan 1

--- jobsheet11/SLLMain17.java
+++ jobsheet11/SLLMain17.java
@@ -1,19 +1,52 @@
+import java.util.Scanner;
+
 public class SLLMain17 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         SingleLinkedList17 sll = new SingleLinkedList17();
-        Mahasiswa17 mhs1 = new Mahasiswa17("24212200", "Alvaro", "1A", 4.0);
-        Mahasiswa17 mhs2 = new Mahasiswa17("23212201", "Bimon", "2B", 3.8);
-        Mahasiswa17 mhs3 = new Mahasiswa17("22212202", "Cintia", "3C", 3.5);
-        Mahasiswa17 mhs4 = new Mahasiswa17("21212203", "Dirga", "4D", 3.6);
+        int pilihan;
 
-        sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addLast(mhs1);
-        sll.print();
-        sll.insertAfter("Dirga", mhs3);
-        sll.insertAt(2, mhs2);
-        sll.print();
+        do {
+             System.out.println("\nMENU:");
+            System.out.println("1. Tambah di Awal");
+            System.out.println("2. Tambah di Akhir");
+            System.out.println("3. Sisipkan Setelah Nama");
+            System.out.println("4. Tampilkan Data");
+            System.out.println("5. Keluar");
+            System.out.print("Pilih opsi: ");
+            pilihan = sc.nextInt();
+            sc.nextLine();
 
+            if (pilihan == 1 || pilihan == 2 || pilihan == 3) {
+                System.out.print("Masukkan NIM: ");
+                String nim = sc.nextLine();
+                System.out.print("Masukkan Nama: ");
+                String nama = sc.nextLine();
+                System.out.print("Masukkan Kelas: ");
+                String kelas = sc.nextLine();
+                System.out.print("Masukkan IPK: ");
+                double ipk = sc.nextDouble();
+                sc.nextLine();
+
+                Mahasiswa17 mhs = new Mahasiswa17(nim, nama, kelas, ipk);
+
+                switch (pilihan) {
+                    case 1:
+                        sll.addFirst(mhs);
+                        break;
+                    case 2:
+                        sll.addLast(mhs);
+                        break;
+                    case 3:
+                        System.out.print("Masukkan Nama Setelah Siapa? ");
+                        String key = sc.nextLine();
+                        sll.insertAfter(key, mhs);
+                        break;
+                }
+            } else if (pilihan == 4) {
+                sll.print();
+            }
+        } while (pilihan != 5);
+        sc.close();
     }
 }
